f4123fd704ae9f0e68fb23c2f34f3b2290eeccd5
Eric Branson <esbranson@gmail.com>
2022-11-29 17:01:23 -0700

    Add tag filter

--- lawapp.js
+++ lawapp.js
@@ -2,6 +2,8 @@ class LawApp extends HTMLElement {
     constructor() {
         super();
 
+        this.XPathResultPolyfill();
+
         // Create a shadow root
         this.attachShadow({ mode: "open" }); // sets and returns 'this.shadowRoot'
 
@@ -9,12 +11,32 @@ class LawApp extends HTMLElement {
             ? this.getAttribute("href")
             : "https://www.legislation.gov.uk/ukpga/1982/11/data.akn";
 
-        this.doFetch(url).then(data => this.shadowRoot.append(data.children[0]));
+        const filter = this.hasAttribute("filter")
+            ? this.getFilter(this.getAttribute("filter"))
+            : $null; // XXX
+
+        this.doFetch(url).then(data => this.doIt(data, filter));
 
         const linkElem = document.createElement("link");
         linkElem.setAttribute("rel", "stylesheet");
         linkElem.setAttribute("href", "akn.css");
         this.shadowRoot.appendChild(linkElem);
+    }
+
+    doIt(data, filter) {
+        this.doFilter(data, filter);
+        this.shadowRoot.append(data.children[0]);
+    }
+
+    doFilter(document, filter) {
+        Array.from(document.evaluate(filter, document, () => "http://docs.oasis-open.org/legaldocml/ns/akn/3.0", XPathResult.ANY_TYPE, null)).map((result) => console.debug(result.remove()));
+    }
+
+    getFilter(filter_list_str) {
+        const list = filter_list_str?.split(/[\s]+/);
+        const xpath = list?.map(str => ('//an:'+str)).join('|');
+        console.debug({xpath});
+        return xpath;
     }
 
     async doFetch(url) {
@@ -24,7 +46,7 @@ class LawApp extends HTMLElement {
             if (response.ok) {
                 const str = await response.text();
                 const data = new window.DOMParser().parseFromString(str, "text/xml");
-                console.debug({data})
+                console.debug({ data })
                 return data;
             } else {
                 console.error(response);
@@ -32,6 +54,26 @@ class LawApp extends HTMLElement {
         } catch (e) {
             console.error(e);
         }
+    }
+
+    // https://stackoverflow.com/questions/47017441/how-to-use-array-from-with-a-xpathresult/72548135#72548135
+    XPathResultPolyfill() {
+        if (!XPathResult.prototype[Symbol.iterator]) XPathResult.prototype[Symbol.iterator] = function* () {
+            switch (this.resultType) {
+                case XPathResult.UNORDERED_NODE_ITERATOR_TYPE:
+                case XPathResult.ORDERED_NODE_ITERATOR_TYPE:
+                    let result;
+                    while ( (result = this.iterateNext()) != null ) yield result;
+                    break;
+                case XPathResult.UNORDERED_NODE_SNAPSHOT_TYPE:
+                case XPathResult.ORDERED_NODE_SNAPSHOT_TYPE:
+                    for (let i=0; i < this.snapshotLength; i++) yield this.snapshotItem(i);
+                    break;
+                default:
+                    yield this.singleNodeValue;
+                    break;
+            }
+        };
     }
 }
 
